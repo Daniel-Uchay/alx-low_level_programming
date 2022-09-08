6806a0a0c9a05ec119080c00a7395f600d2cfb5c
Daniel-Uchay <joinuchedaniel@gmail.com>
2022-09-08 16:09:39 -0700

    return 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main : Entry point:
+ * Explation: return 1
+ * Return: None
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
