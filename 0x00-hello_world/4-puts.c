e5704313d56835a47c26cdeeb8d7858daadffe36
Daniel-Uchay <joinuchedaniel@gmail.com>
2022-09-08 15:42:59 -0700

    PutS

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entery point of c program
+ * Description: 'Print ising puts'
+ * Return: Always 0 if success
+ */
+int main(void) /* main */
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0); /* return value */
+}
+
