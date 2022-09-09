e6ba0f0847824a95bda3c066fd0ca0db7a8af75a
Daniel-Uchay <joinuchedaniel@gmail.com>
2022-09-09 16:09:10 -0700

    base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: base 10 starting
+ * from 0
+ * Return: o
+ */
+int main(void)
+{
+	int i; 
+
+	for (i = 0; x <= 9; x++)
+	{
+		putchar((x % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
+
