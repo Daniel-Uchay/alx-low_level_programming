c9e4f0931b806c8e77c1e489ee1ca4e1e7f434ba
Daniel-Uchay <joinuchedaniel@gmail.com>
2022-09-15 15:02:41 -0700

    print square

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -17,4 +15,3 @@ void print_triangle(int size);
 void print_number(int n);
 
 #endif
-
