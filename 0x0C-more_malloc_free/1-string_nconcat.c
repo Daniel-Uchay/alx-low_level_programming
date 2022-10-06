8bd90efbf053a64da9c973ed87f85236270e0dbf
Daniel-Uchay <meetcaxtroh@gmail.com>
2022-10-06 23:19:56 +0100

    edited

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,38 +1,38 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * string_nconcat - function concatenates two strings
- * @s1: first string to be concatenated
- * @s2: second string to be concatenated
- * @n: size to be allocated
- * Return: pointer
+ * string_nconcat - function that concatenates two strings
+ * @s1: string one being passed
+ * @s2: string two being passed
+ * @n: size
+ * Return: Always 0.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len1, len2, len;
 	char *p;
+	unsigned int a, b, c, d;
 
-	if (s1 == NULL)
-		return ("");
-	if (s2 == NULL)
-		return ("");
-	for (i = 0; s1[i]; i++)
-		len1++;
-	for (i = 0; s2[i]; i++)
-		len2++;
-	if (n > len2)
-		len = len1 + len2;
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
+	for (a = 0; s1[a] != '\0'; a++)
+		;
+	for (b = 0; s2[b] != '\0'; b++)
+		;
+	if (n > b)
+		p = malloc((a + b + 1) * sizeof(char));
 	else
-		len = len1 + n;
-	p = malloc((sizeof(char) * len + 1));
-	if (p == NULL)
-	{
+		p = malloc((a + n + 1) * sizeof(char));
+	if (p == 0)
 		return (NULL);
-	}
-	j = 0;
-	for (i = 0; s1[i]; i++)
-		p[j++] = s1[i];
-	for (i = 0; s2[i] && i < n; i++)
-		p[j++] = s2[i];
+
+	for (c = 0; c < a; c++)
+		p[c] = s1[c];
+	for (d = 0; d < n && d < b; d++, c++)
+		p[c] = s2[d];
+	p[c] = '\0';
 	return (p);
 }
-
